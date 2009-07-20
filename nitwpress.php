6168c2cd6194d14b420ce3be81e0105f21edb07f
sakuratan <sakuratan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-07-20 06:00:43 +0000

    Missing textdomain with __ bug fixed.
    git-svn-id: https://plugins.svn.wordpress.org/nitwpress/trunk@136876 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- nitwpress.php
+++ nitwpress.php
@@ -264,11 +264,11 @@ function nitwpress_widget_control() {
 <?php
 
     if (!is_dir(NITWPRESS_CACHEDIR)) {
-	nitwpress_display_error(sprintf(__("Missing permissions for writing on %s. Fix the error before enter your Twitter account."), NITWPRESS_CACHEDIR));
+	nitwpress_display_error(sprintf(__("Missing permissions for writing on %s. Fix the error before enter your Twitter account.", 'nitwpress'), NITWPRESS_CACHEDIR));
     }
 
     if (!function_exists('curl_init')) {
-	nitwpress_display_error(__('Missing cURL module.'));
+	nitwpress_display_error(__('Missing cURL module.', 'nitwpress'));
     }
 }
 
@@ -308,7 +308,7 @@ function nitwpress_add_cron($sched) {
 	if (array_key_exists('interval', $options)) {
 	    $sched['nitwpress'] = array(
 		'interval' => (int)$options['interval'] * 60,
-		'display' => __('Schedule for NiTwPress plugins')
+		'display' => __('Schedule for NiTwPress plugins', 'nitwpress')
 	    );
 	}
     }
